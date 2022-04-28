ed429bde39baf9e35b8c760b89db8ca35cec6836
mateusz5564 <mateusz5564@gmail.com>
2022-04-28 22:47:08 +0200

    unicafe add step3 solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,15 +2,30 @@ import { useState } from "react";
 
 const Button = ({ onClick, children }) => <button onClick={onClick}>{children}</button>;
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positivePercent = (good / all) * 100;
+
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercent}%</p>
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const handleButtonClick = (state, setState) => () => setState(state + 1);
-  const all = good + neutral + bad;
-  const average = (good * 1 + bad * -1) / all;
-  const positivePercent = good / all * 100;
 
   return (
     <div>
@@ -19,13 +34,7 @@ const App = () => {
       <Button onClick={handleButtonClick(neutral, setNeutral)}>neutral</Button>
       <Button onClick={handleButtonClick(bad, setBad)}>bad</Button>
 
-      <h2>statistics</h2>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercent}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 };
