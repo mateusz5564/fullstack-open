250377a15dd108401ead6de21159752ffbcb4f37
mateusz5564 <mateusz5564@gmail.com>
2022-04-29 12:43:00 +0200

    unicafe add step5 solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,26 +2,32 @@ import { useState } from "react";
 
 const Button = ({ onClick, children }) => <button onClick={onClick}>{children}</button>;
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = (good * 1 + bad * -1) / all;
-  const positivePercent = (good / all) * 100;
+  const positivePercent = `${(good / all) * 100} %`;
 
   if (all === 0) {
-    return (
-      <p>No feedback given</p>
-    )
+    return <p>No feedback given</p>;
   }
 
   return (
     <>
       <h2>statistics</h2>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercent}%</p>
+      <StatisticLine text={"good"} value={good} />
+      <StatisticLine text={"neutral"} value={neutral} />
+      <StatisticLine text={"bad"} value={bad} />
+      <StatisticLine text={"all"} value={all} />
+      <StatisticLine text={"average"} value={average} />
+      <StatisticLine text={"positive"} value={positivePercent} />
     </>
   );
 };
@@ -40,7 +46,7 @@ const App = () => {
       <Button onClick={handleButtonClick(neutral, setNeutral)}>neutral</Button>
       <Button onClick={handleButtonClick(bad, setBad)}>bad</Button>
 
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
