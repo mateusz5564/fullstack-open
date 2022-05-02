dc0290af1118ea65a2d70f251f04438df3823c24
mateusz5564 <mateusz5564@gmail.com>
2022-05-02 18:26:19 +0200

    the-phonebook add step2 and step3 solution

---
+++ part2/the-phonebook/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: 123456789 }]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const addPerson = e => {
+    e.preventDefault();
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
