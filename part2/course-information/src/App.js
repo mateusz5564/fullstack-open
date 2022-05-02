f401eca36b3bf20c67bce040a2e258dec06eb8a7
mateusz5564 <mateusz5564@gmail.com>
2022-05-02 12:26:35 +0200

    course-information add step6 solution

---
+++ part2/course-information/src/App.js
@@ -0,0 +1,28 @@
+import Course from "./components/Course";
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <Course course={course} />
+  );
+};
+
+
+export default App;
