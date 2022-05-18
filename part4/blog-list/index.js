f43a88881da56dc0c5f1b232888679850496eb3c
mateusz5564 <mateusz5564@gmail.com>
2022-05-18 14:24:12 +0200

    blog list step2 create blog controller module

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const BlogRouter = require("./controllers/blog");
 const { PORT, MONGODB_URI } = require('./utils/config');
 const { info } = require('./utils/logger.js');
 
@@ -11,20 +11,7 @@ mongoose.connect(MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', BlogRouter)
 
 app.listen(PORT, () => {
   info(`Server running on port ${PORT}`);
