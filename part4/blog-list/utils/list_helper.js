58d6e481fc1c0a849b3b5287f0cf8a9badf06cd6
mateusz5564 <mateusz5564@gmail.com>
2022-05-18 21:14:55 +0200

    helper functons and unit tests step3 solution

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -3,10 +3,29 @@ const dummy = blogs => {
 };
 
 const totalLikes = blogs => {
-  return blogs.reduce((acc, current) => acc + current.likes, 0)
+  return blogs.reduce((acc, current) => acc + current.likes, 0);
+};
+
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null;
+
+  let favoriteBlog = blogs[0];
+
+  blogs.forEach(blog => {
+    if (blog.likes > favoriteBlog.likes) {
+      favoriteBlog = blog;
+    }
+  });
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes,
+  };
 };
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
