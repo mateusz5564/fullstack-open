7d05ba736e9f50059c080461f144c0cfb88c2e05
mateusz5564 <mateusz5564@gmail.com>
2022-05-30 21:58:38 +0200

    bloglist e2e testing step1 solution

---
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,10 @@
+describe("Blog app", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.visit("http://localhost:3000");
+  });
+
+  it("displays the login form by default", function () {
+    cy.contains("login");
+  });
+});
