7552536f0ea346d425bebdfb2d406b73148d33ee
mateusz5564 <mateusz5564@gmail.com>
2022-06-03 15:36:59 +0200

    6.15 anecdotes solution

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getAll } from "./services/anecdotes";
-import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { getAnecdotes } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
@@ -11,9 +10,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getAll().then(data => {
-      dispatch(setAnecdotes(data));
-    });
+    dispatch(getAnecdotes());
   }, [dispatch]);
 
   return (
--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getAll } from "../services/anecdotes";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
+const initialState = [];
 
 const anecdoteSlice = createSlice({
   name: "anecdote",
@@ -40,6 +22,13 @@ const anecdoteSlice = createSlice({
     },
   },
 });
+
+export const getAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await getAll();
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export const { setAnecdotes, voteForAnecdote, addAnecdote } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
