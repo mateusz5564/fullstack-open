ae3c207fc5019dd1befd9be5d340c69ef040be2d
mateusz5564 <mateusz5564@gmail.com>
2022-06-30 23:37:28 +0200

    9.12 Patientor backend, step5

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,18 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import express from "express";
 const router = express.Router();
 
 import patientService from "../services/patients";
 
 router.get("/", (_req, res) => {
   return res.json(patientService.getNoSsnEntries());
+});
+
+router.post("/", (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatient = patientService.addEntry({ name, dateOfBirth, ssn, gender, occupation });
+
+  res.json(newPatient);
 });
 
 export default router;
--- part9/patientor-backend/src/services/patients.ts
+++ part9/patientor-backend/src/services/patients.ts
@@ -1,5 +1,6 @@
+import { v4 as uuid } from 'uuid';
 import patients from "../../data/patients";
-import { Patient, PatientNoSsn } from "../types";
+import { NewPatient, Patient, PatientNoSsn } from "../types";
 
 const getEntries = (): Array<Patient> => {
   return patients;
@@ -7,11 +8,26 @@ const getEntries = (): Array<Patient> => {
 
 const getNoSsnEntries = (): Array<PatientNoSsn> => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
-    id, name, dateOfBirth, gender, occupation
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
   }));
 };
 
+const addEntry = (patient: NewPatient): Patient => {
+  const patientWithId = {
+    id: uuid(),
+    ...patient,
+  };
+  patients.push(patientWithId);
+
+  return patientWithId;
+};
+
 export default {
+  addEntry,
   getEntries,
   getNoSsnEntries,
 };
